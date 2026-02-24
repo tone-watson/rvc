a4c5a551a1f552583e150112b98841c89be1810d
cooljerk <cooljerk@noiseflood.com>
2026-02-24 11:54:08 +0000

    Fixes

--- infer/lib/train/utils.py
+++ infer/lib/train/utils.py
@@ -235,8 +235,7 @@ def plot_spectrogram_to_numpy(spectrogram):
     plt.tight_layout()
 
     fig.canvas.draw()
-    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep="")
-    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    data = np.asarray(fig.canvas.buffer_rgba())[:, :, :3]
     plt.close()
     return data
 
@@ -266,8 +265,7 @@ def plot_alignment_to_numpy(alignment, info=None):
     plt.tight_layout()
 
     fig.canvas.draw()
-    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep="")
-    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    data = np.asarray(fig.canvas.buffer_rgba())[:, :, :3]
     plt.close()
     return data
 
